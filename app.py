d3072382bfac72748c28a97bbc1e110ece7e3e39
Theriley106 <christopherlambert106@gmail.com>
2018-03-03 11:58:03 -0500

    Added search user

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route('/searchUser/<firstName><lastName>')
+def searchUser(firstName, lastName):
+	return "<h1>{} {}</h1>".format(firstName, lastName)
+
 
 if __name__ == "__main__":
 	app.run()
