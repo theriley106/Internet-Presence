ee30b45fa87a2451701412658c4a3d76200a41f8
Theriley106 <christopherlambert106@gmail.com>
2018-03-04 02:12:43 -0500

    Return jsonfiy

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ def index():
 	return render_template("index.html")
 
 @app.route('/addCode/<code>')
-def index(code):
-	return render_template("index.html")
+def addCode(code):
+	return jsonify(convertCode(code))
 
 def extractLastName(code):
 	# /add/%40%0A%1EANSI+636005050001DL003100272DCAD%0ADCBU%0ADCD%0ADBA06102020%0ADCSLAMBERT%0ADACCHRISTOPHER%0ADADROBERT%0ADBD04092015%0ADBB06101999%0ADBC1%0ADAU075+in%0ADAG17+ENGEL+DR%0ADAIGREENVILLE%0ADAJSC%0ADAK296177209%0ADAQ103659059%0ADCF1036590590409201510%0ADCGUSA%0ADDEU%0ADDFU%0ADDGU%0ADCU%0ADAW240%0ADCRU+-+Conditional+%2FProvisional++++++++++++++++++++++%C2%85%C2%85/glutenFree
